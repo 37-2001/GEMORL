ab3f5cc18982e37ed853fc3ed74e505998f71f8c
科学革命统一 <13248275705@163.com>
2025-12-27 14:26:29 +0800

    Update actor_agent13.py

--- actor_agent13.py
+++ actor_agent13.py
@@ -3,8 +3,8 @@ import tensorflow.contrib.layers as tl
 from state_normalization import normalize_node_inputs, normalize_job_inputs
 from tf_op import *
 from msg_passing_path import *
-from gcn3 import GraphCNN
-from gsn3 import GraphSNN
+from gcn import GraphCNN
+from gsn import GraphSNN
 from agent import Agent
 from  sklearn.preprocessing import  StandardScaler
 
@@ -420,4 +420,5 @@ class ActorAgent3(Agent):
                gcn_mats, gcn_masks, summ_mats, \
                running_dags_mat, dag_summ_backward_map, \
                job_dags_changed
+
 
